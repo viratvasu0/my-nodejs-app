055a2bfc00c0036dd3179c03b783d31f1d5d480c
viratvasu0 <136569031+viratvasu0@users.noreply.github.com>
2026-06-20 18:15:43 +0530

    Correct log message for server start
    Fix typo in server running message.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running ovhvhn port ${port}`);
 });
